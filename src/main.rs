336b77e2d373cc81e4b75b209a57580ae26d4c1b
Mario Prince <mario.prince@outlook.com>
2017-06-21 20:17:12 -0400

    assert

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,22 @@
 // A translation of Peter Norvig’s Sudoku solver from Python to Rust     http://www.norvig.com/sudoku.html
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+
+fn cross (rows : &[char], cols : &[char]) -> Vec<String> {
+    let mut v = Vec::new();
+    for ch in rows {
+        for d in cols {
+            let mut sq = String::new();
+            sq.push(*ch);
+            sq.push(*d);
+            v.push(sq)
+        }
+    }
+    v
+}
 
 fn main() {
     let cols : Vec<char> = "123456789".chars().collect();
     let rows : Vec<char> = "ABCDEFGHI".chars().collect();
-    
-    fn cross (rows : &[char], cols : &[char]) -> Vec<String> {
-        let mut v = Vec::new();
-        for ch in rows {
-            for d in cols {
-                let mut sq = String::new();
-                sq.push(*ch);
-                sq.push(*d);
-                v.push(sq)
-            }
-        }
-        v
-    }
-
     let squares = cross(&rows, &cols);
 
     // A vector of units (a unit = a column, a row or a box of 9 squares)
@@ -45,4 +44,24 @@ fn main() {
     }
   
     //  peers is a dictionary where each square s maps to the set of squares formed by the union of the squares in the units of s, but not s itself 
+    let mut peers = HashMap::<String, Vec<String>>::new();
+    for s in &squares {
+        let peers_set : HashSet<String> = units[s].concat().clone().into_iter().filter(|p| p != s).collect();
+        let mut peers_vec = Vec::<String>::new(); 
+        peers_vec.extend(peers_set.into_iter());
+        peers.insert(s.clone(), peers_vec);   
+    }
+
+    //  A set of unit tests.
+    assert_eq!(squares.len(), 81);
+    assert_eq!(unitlist.len(), 27);
+    assert!(squares.iter().all(|s| units[s].len() == 3));
+    assert!(squares.iter().all(|s| peers[s].len() == 20));
+    assert_eq!(units.get("C2"), Some(vec![vec!["A2", "B2", "C2", "D2", "E2", "F2", "G2", "H2", "I2"],
+                                          vec!["C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9"],
+                                          vec!["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]]));
+    assert_eq!(peers.get("C2"), Some(vec!["A2", "B2", "D2", "E2", "F2", "G2", "H2", "I2",
+                                          "C1", "C3", "C4", "C5", "C6", "C7", "C8", "C9",
+                                          "A1", "A3", "B1", "B3"]));
+    println!("All tests pass.");
 }
