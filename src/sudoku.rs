e8ee9b785ac4072c3b59d57bbf7de47c3c4f2c23
Rrogntudju <rrogntudju@example.com>
2021-08-03 14:14:41 -0400

    réorg

--- src/sudoku.rs
+++ src/sudoku.rs
@@ -1,16 +1,8 @@
 // From Peter Norvig’s Sudoku solver     http://www.norvig.com/sudoku.html
-use std::collections::{HashMap};
+use std::collections::HashMap;
 use std::fmt;
-
-#[derive(Debug)]
-struct Context {
-    cols: Vec<char>,
-    rows: Vec<char>,
-    squares: Vec<String>,
-    unitlist: Vec<Vec<String>>,
-    units: HashMap<String, Vec<Vec<String>>>,
-    peers: HashMap<String, Vec<String>>
-}
+use rand::rngs::ThreadRng;
+use rand::seq::SliceRandom;
 
 fn cross (rows: &[char], cols: &[char]) -> Vec<String> {
     let mut v = Vec::new();
@@ -44,16 +36,18 @@ impl fmt::Display for PuzzleError {
 
 type PuzzleResult<T> = Result<T, PuzzleError>;
 
+#[derive(Debug)]
 pub struct Sudoku {
-    ctx: Context
+    cols: Vec<char>,
+    rows: Vec<char>,
+    squares: Vec<String>,
+    unitlist: Vec<Vec<String>>,
+    units: HashMap<String, Vec<Vec<String>>>,
+    peers: HashMap<String, Vec<String>>
 }
 
 impl Sudoku {
-    pub fn new () -> Sudoku {
-        Sudoku { ctx: Sudoku::context_init() }
-    }
-
-    fn context_init() -> Context {
+    pub fn new () -> Self {
         let cols: Vec<char> = "123456789".chars().collect();
         let rows: Vec<char> = "ABCDEFGHI".chars().collect();
         let squares = cross(&rows, &cols);
@@ -88,16 +82,16 @@ impl Sudoku {
             peers.insert(s.clone(), peers_s);   
         }
 
-        Context {cols: cols, rows: rows, squares: squares, unitlist: unitlist, units: units, peers: peers}
+        Self {cols, rows, squares, unitlist, units, peers}
     }
 
 
     fn grid_values (&self, grid: &str) -> PuzzleResult<HashMap<String, Vec<char>>> {
         //  Convert grid into a dict of (square, char Vec) with '0' or '.' for empties.
-        let grid_chars: Vec<Vec<char>> = grid.chars().filter(|ch| self.ctx.cols.contains(ch) || ['0', '.'].contains(ch)).map(|ch| vec![ch]).collect();
+        let grid_chars: Vec<Vec<char>> = grid.chars().filter(|ch| self.cols.contains(ch) || ['0', '.'].contains(ch)).map(|ch| vec![ch]).collect();
         if grid_chars.len() == 81 {
             let mut grid_values = HashMap::<String, Vec<char>>::new();
-            grid_values.extend(self.ctx.squares.iter().cloned().zip(grid_chars.into_iter()));
+            grid_values.extend(self.squares.iter().cloned().zip(grid_chars.into_iter()));
             Ok(grid_values)
         } else {
             Err(PuzzleError::InvalidGrid)
@@ -107,13 +101,13 @@ impl Sudoku {
     fn parse_grid (&self, grid: &str) -> PuzzleResult<HashMap<String, Vec<char>>> {
         //  Convert grid to Some dict of possible values, [square, digits], or return None if a contradiction is detected.
         let mut values = HashMap::<String, Vec<char>>::with_capacity(81);
-        for s in &self.ctx.squares { 
-            values.insert(s.clone(), self.ctx.cols.clone());
+        for s in &self.squares { 
+            values.insert(s.clone(), self.cols.clone());
         }
         let grid_values = self.grid_values(grid)?;
         for (s, gvalues) in &grid_values {
             for d in gvalues {
-                if self.ctx.cols.contains(d) && !self.assign(&mut values, s, d) {
+                if self.cols.contains(d) && !self.assign(&mut values, s, d) {
                     return Err(PuzzleError::Contradiction);
                 }
             }
@@ -138,11 +132,11 @@ impl Sudoku {
         if d2.is_empty() {
             return false; // Contradiction: removed last value
         } 
-        if d2.len() == 1 && !self.ctx.peers[s].iter().all(|s2| self.eliminate(values, s2, &d2[0])) {
+        if d2.len() == 1 && !self.peers[s].iter().all(|s2| self.eliminate(values, s2, &d2[0])) {
             return false;
         }
         // (rule 2) If a unit u is reduced to only one place for a value d, then put it there.
-        for u in &self.ctx.units[s] {
+        for u in &self.units[s] {
             let dplaces: Vec<String> = u.iter().cloned().filter(|s2| values[s2].contains(d)).collect();
             if dplaces.is_empty() {
                 return false;   // Contradiction: no place for this value
@@ -176,7 +170,7 @@ impl Sudoku {
     pub fn solve (&self, grid: &str) -> PuzzleResult<String> {
         let values = self.parse_grid(grid).and_then(|v| {self.search(v)})?;
         if self.solved(&values) {
-            Ok(self.ctx.squares.iter().map(|s| {values[s][0]}).collect::<String>())
+            Ok(self.squares.iter().map(|s| {values[s][0]}).collect::<String>())
         } else {
             Err(PuzzleError::Unsolved)
         }
@@ -187,9 +181,9 @@ impl Sudoku {
         let unitsolved = |unit: &Vec<String>| {
             let mut digits_values = unit.iter().map(|s| values[s].iter().collect::<String>()).collect::<Vec<String>>();
             digits_values.sort();
-            digits_values == self.ctx.cols.iter().map(char::to_string).collect::<Vec<String>>()
+            digits_values == self.cols.iter().map(char::to_string).collect::<Vec<String>>()
         };
-        self.ctx.unitlist.iter().all(unitsolved)
+        self.unitlist.iter().all(unitsolved)
     }  
 
     pub fn display (grid: &str) -> PuzzleResult<Vec<String>> {
@@ -215,4 +209,111 @@ impl Sudoku {
             Err(PuzzleError::InvalidGrid)
         }
     }
+
+    pub fn random_puzzle(&self, n: usize, rng: &mut ThreadRng) -> String {
+        /*  Make a random puzzle with N or more assignments. Restart on contradictions.
+            Note the resulting puzzle is not guaranteed to be solvable, but empirically
+            about 99.8% of them are solvable. Some have multiple solutions. */
+        let mut values = HashMap::<String, Vec<char>>::with_capacity(81);
+        for s in &self.squares {
+            values.insert(s.clone(), self.cols.clone());
+        }
+        let mut squares = self.squares.clone();
+        squares.shuffle(rng);
+        for s in &squares {
+            let d2 = values[s].clone();
+            if !self.assign(&mut values, s, d2.choose(rng).unwrap()) {
+                break;
+            }
+            let mut ds: Vec<Vec<char>> = values.iter().filter(|&(_, v)| v.len() == 1).map(|(_, v)| v.clone()).collect();
+            if ds.len() >= n {
+                ds.sort();
+                ds.dedup();
+                if ds.len() >= 8 {
+                    return 
+                        self.squares
+                        .iter()
+                        .map(|s| if values[s].len() == 1 { values[s][0] } else { '.' })
+                        .collect::<String>();
+                }
+            }
+        }
+        self.random_puzzle(17, rng) // Give up and make a new puzzle
+    }
+
+    #[cfg(debug_assertions)]
+    pub fn test(&self) -> () {
+        // A set of unit tests.
+        assert_eq!(self.squares.len(), 81);
+        assert_eq!(self.unitlist.len(), 27);
+        assert!(self.squares.iter().all(|s| self.units[s].len() == 3));
+        assert!(self.squares.iter().all(|s| self.peers[s].len() == 20));
+        assert_eq!(
+            self.units.get("C2"),
+            Some(&vec![
+                vec![
+                    "A2".into(),
+                    "B2".into(),
+                    "C2".into(),
+                    "D2".into(),
+                    "E2".into(),
+                    "F2".into(),
+                    "G2".into(),
+                    "H2".into(),
+                    "I2".into()
+                ],
+                vec![
+                    "C1".into(),
+                    "C2".into(),
+                    "C3".into(),
+                    "C4".into(),
+                    "C5".into(),
+                    "C6".into(),
+                    "C7".into(),
+                    "C8".into(),
+                    "C9".into()
+                ],
+                vec![
+                    "A1".into(),
+                    "A2".into(),
+                    "A3".into(),
+                    "B1".into(),
+                    "B2".into(),
+                    "B3".into(),
+                    "C1".into(),
+                    "C2".into(),
+                    "C3".into()
+                ]
+            ])
+        );
+    
+        let mut peers_c2 = vec![
+            "A2".into(),
+            "B2".into(),
+            "D2".into(),
+            "E2".into(),
+            "F2".into(),
+            "G2".into(),
+            "H2".into(),
+            "I2".into(),
+            "C1".into(),
+            "C3".into(),
+            "C4".into(),
+            "C5".into(),
+            "C6".into(),
+            "C7".into(),
+            "C8".into(),
+            "C9".into(),
+            "A1".into(),
+            "A3".into(),
+            "B1".into(),
+            "B3".into(),
+        ];
+        peers_c2.sort();
+        assert_eq!(self.peers.get("C2"), Some(&peers_c2));
+        println!("All tests passed!");
+    }
 }
+
+
+    
